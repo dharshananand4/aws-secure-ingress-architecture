3f9ccaf4eb93c606b283c89e6a9519c45ec28bda
dharshananand4 <dharshananand4@gmail.com>
2026-07-17 21:34:39 +0530

    Create security groups for ALB and app tier
    Added security groups for ALB and application tier with appropriate ingress and egress rules.

---
+++ Security_groups.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "alb" {
+  name        = "alb-security-group"
+  description = "Allow inbound public web traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "app" {
+  name        = "app-security-group"
+  description = "Isolate application tier, allow only ALB access"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
